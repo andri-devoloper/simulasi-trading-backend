fa8427d2d71da6d83dbbf2896e5de9ba731c727a
4ndr125 <107790299+andri-devoloper@users.noreply.github.com>
2025-06-13 20:33:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
 const cors = require("cors");
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 const routes = require("./routes/routes");
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
